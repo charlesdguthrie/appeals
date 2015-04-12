4ca2e56ce0ad5287deed1352ce7ada59de75c60a
Charlie Guthrie <charlesdguthrie@gmail.com>
2015-04-12 17:40:19 -0400

    converted tabs to 4 spaces

--- scripts/extract_metadata.py
+++ scripts/extract_metadata.py
@@ -1,31 +1,31 @@
 import pandas as pd
 
 def extract_metadata(inpath):
-	'''
-	extracts list of judges, their vote valences, and whether they voted with the majority;
-	as well as information about their decisions from case
+    '''
+    extracts list of judges, their vote valences, and whether they voted with the majority;
+    as well as information about their decisions from case
 
-	arguments: path of input file (ex: 'data/merged_caselevel_data.csv')
+    arguments: path of input file (ex: 'data/merged_caselevel_data.csv')
 
-	returns: dataframe with only the relevant columns
+    returns: dataframe with only the relevant columns
 
-	inputs: inpath 
-	
-	'''
-	df = pd.read_csv(inpath)
+    inputs: inpath 
+    
+    '''
+    df = pd.read_csv(inpath)
 
-	#extract list of judges, their vote valences, and whether they voted with the majority
-	judges = df.iloc[:,160:229]
+    #extract list of judges, their vote valences, and whether they voted with the majority
+    judges = df.iloc[:,160:229]
 
-	#extract info about the decisions, starting with case ID and majority vote (direct1) 
-	decisions = df.loc[:,('caseid','direct1','geniss','casetyp1','treat','majvotes','dissent','concur','casetyp2','direct2')]
+    #extract info about the decisions, starting with case ID and majority vote (direct1) 
+    decisions = df.loc[:,('caseid','direct1','geniss','casetyp1','treat','majvotes','dissent','concur','casetyp2','direct2')]
 
-	#merge judges and decisions
-	df2 = pd.concat([decisions,judges],axis=1)
-	return df2
+    #merge judges and decisions
+    df2 = pd.concat([decisions,judges],axis=1)
+    return df2
 
 if __name__ == "__main__": 
-	df = extract_metadata('data/merged_caselevel_data.csv')
-	outpath = 'data/metadata_compact.csv'
-	df.to_csv(outpath,index=False)
-	print "File saved to %s" %outpath
+    df = extract_metadata('data/merged_caselevel_data.csv')
+    outpath = 'data/metadata_compact.csv'
+    df.to_csv(outpath,index=False)
+    print "File saved to %s" %outpath
