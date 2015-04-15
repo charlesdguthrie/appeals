09396d4d8976f86d5ff91f6c5c999cfbedb48f27
Charlie Guthrie <charlesdguthrie@gmail.com>
2015-04-14 20:07:42 -0400

    one-hot encoded labels into liberal,conservative,mixed, and unknown

--- scripts/extract_metadata.py
+++ scripts/extract_metadata.py
@@ -18,11 +18,23 @@ def extract_metadata(inpath):
     assert df.shape[1]>160, "df is wrong shape"
     judges = df.iloc[:,160:229]
 
-    #extract info about the decisions, starting with case ID and majority vote (direct1) 
-    decisions = df.loc[:,('caseid','direct1','geniss','casetyp1','treat','majvotes','dissent','concur','casetyp2','direct2')]
+    #extract caseID and create classification labels
+    y = df.loc[:,('caseid','direct1')]
+    y['liberal'] = 0
+    y.loc[y['direct1']==3,'liberal']=1
+    y['conservative'] = 0
+    y.loc[y['direct1']==1,'conservative']=1
+    y['mixed'] = 0
+    y.loc[y['direct1']==2,'mixed']=1
+    y['unknown'] = 0
+    y.loc[y['direct1']==0,'unknown']=1
+    y.head()
 
-    #merge judges and decisions
-    df2 = pd.concat([decisions,judges],axis=1)
+    #extract other info about the decisions, starting with case ID and majority vote (direct1) 
+    decisions = df.loc[:,('geniss','casetyp1','treat','majvotes','dissent','concur','casetyp2','direct2')]
+
+    #merge labels, judges and decisions
+    df2 = pd.concat([y,decisions,judges],axis=1)
     return df2
 
 if __name__ == "__main__": 
