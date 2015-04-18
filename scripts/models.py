97a9dfc86e8443e71c9f03f40108cf098038c4ca
Charlie Guthrie <charlesdguthrie@gmail.com>
2015-04-18 16:15:54 -0400

    Generalized evaluate_accuracy to work for multiclass classification

--- scripts/models.py
+++ scripts/models.py
@@ -1,4 +1,4 @@
-import pandas as pd
+ import pandas as pd
 from sklearn.metrics import confusion_matrix
 
 class MajorityClassifier:
@@ -13,11 +13,33 @@ class MajorityClassifier:
     def predict(self,X_test):
         return [self.majority] * X_test.shape[0]
 
-def evaluate_accuracy(y_test,y_pred):
-    cm = confusion_matrix(y_test,y_pred)
+def evaluate_accuracy(y_true,y_pred):
+    '''
+    Prints out confusion matrix and returns percent accuracy
+
+    Args:
+        y_true: array of y values 
+        y_pred: array of predicted y values
+
+    Returns:
+        percent of cases where y_pred == y_true
+    '''
+    cm = confusion_matrix(y_true,y_pred)
+    height = cm.shape[0]
+    width = cm.shape[1]
+    uniques = np.unique(np.array([y_true,y_pred]))
+    header = ""
+    for u in uniques:
+        header = header + " \t " + str(int(u))
+
     print "\t \t pred"
-    print "\t \t 0 \t 1"
-    print "true \t 0 \t %d \t %d" %(cm[0][0],cm[0][1])
-    print "\t 1 \t %d \t %d" %(cm[1][0],cm[1][1])
+    print "true \t" + header
+    for i,u in enumerate(uniques):
+        row = ""
+        for j in range(len(uniques)):
+            row = row + " \t " + str(cm[i][j])
+        print "\t " + str(int(u)) + row
     
-    return (cm[0][0] + cm[1][1])*1.0/len(y_test)
+    return (np.diagonal(cm).sum())*1.0/len(y_true)
+
+#TODO: Add real classifiers using the real data
