8c880dc3b49f3e29e3db267bb468becc39083028
pinesol <alpinesol@gmail.com>
2015-05-02 23:00:07 -0400

    added feature selection

--- scripts/models.py
+++ scripts/models.py
@@ -6,9 +6,10 @@ import sys
 import extract_metadata
 import join_data as jd
 
+from sklearn.feature_selection import chi2, SelectFpr
 from sklearn.pipeline import Pipeline
-from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 import sklearn.metrics
 from sklearn.grid_search import GridSearchCV
 from sklearn.naive_bayes import BernoulliNB, MultinomialNB
@@ -66,7 +67,7 @@ def print_accuracy_info(y_pred, y_true):
 
 
 def train_and_score_model(X, y, case_ids, model,
-                          train_pct, reg_min_log10, reg_max_log10, scoring, regularization_type):
+                          train_pct, reg_min_log10, reg_max_log10, scoring, feature_reduction_type):
     '''
     Train and score a model
     Args:
@@ -75,7 +76,7 @@ def train_and_score_model(X, y, case_ids, model,
         train_pct: float between 0 and 1.  Fraction of your subsample to use as training.
         reg_min_log10, reg_max_log10: integers.  The low and high of your grid search for regularization parameter.
         scoring: scoring method
-        regularization_type: the type of regularation to use. 'l1', 'l2' or None.
+        feature_reduction_type: Either 'chi2', 'l1svc', or None.
 
     Returns:
         Model score
@@ -91,28 +92,39 @@ def train_and_score_model(X, y, case_ids, model,
     print '  Number of features', X.shape[1]
     print 'Training percentage', train_pct
     print 'Scoring used:', scoring
-    print 'Regularization type:', regularization_type
     if reg_min_log10 and reg_max_log10:
         print 'Regularization bounded between 10^(%d) and 10^(%d):' % (
             reg_min_log10, reg_max_log10)
 
     print 'Splitting data into training and testing...'
     X_train, y_train, case_ids_train, X_test, y_test, case_ids_test = train_test_split(X, y, case_ids, train_pct)
-    
-    print 'Fitting model...'
+
+    pipeline_steps = list()
+    param_grid = dict()
+
+    # Feature reduction step
+    if feature_reduction_type == 'chi2':
+        pipeline_steps.append(('feature_reduction', SelectFpr(chi2)))
+        param_grid['feature_reduction__alpha'] = [0.6, 0.8, 1.0] # TODO check where this ends up, expand around where it lands
+    elif feature_reduction_type == 'l1svc':
+        # TODO we can modify the stopping point with 'tol'
+        pipeline_steps.append(('feature_reduction', LinearSVC(penalty="l1", dual=False)))
+        param_grid['feature_reduction__C'] = [10**i for i in range(reg_min_log10, reg_max_log10 + 1)] # TODO check where this ends up, expand around where it lands
 
     if model == 'baseline':
+        # TODO This isn't taking part in feature reduction!
         fitted_model = MajorityClassifier()
         fitted_model.fit(X_train, y_train)
     else:
-        param_grid = dict()
         if model == 'svm':
             # random_state=0 so it always has the same seed so we get deterministic results.
             # Using dual=False b.c. there are lots of features.
-            classifier = LinearSVC(penalty=regularization_type, random_state=0, dual=False)
+            classifier = LinearSVC(penalty='l2', random_state=0, dual=False)
+#            param_grid['classifier__penalty'] = ['l1', 'l2'] # TODO this makes it redoc slow
             param_grid['classifier__C'] = [10**i for i in range(reg_min_log10, reg_max_log10 + 1)]
         elif model == 'logistic':
-            classifier = LogisticRegression(penalty=regularization_type)
+            classifier = LogisticRegression(penalty='l2')
+#            param_grid['classifier__penalty'] = ['l1', 'l2'] # TODO this makes it redonc slow
             param_grid['classifier__C'] = [10**i for i in range(reg_min_log10, reg_max_log10 + 1)]
         elif model == 'naive_bayes':
             # TODO why fit_prior? why not?
@@ -124,8 +136,9 @@ def train_and_score_model(X, y, case_ids, model,
         else:
             print "ERROR: model unknown"
             return
-        pipeline_steps = [('classifier', classifier)]
+        pipeline_steps.append(('classifier', classifier))
 
+        print 'Running Model Pipeline...'
         fitted_model = GridSearchCV(Pipeline(pipeline_steps), scoring=scoring, param_grid=param_grid, 
                                     verbose=1, n_jobs=-1)
         fitted_model.fit(X_train, y_train)
@@ -148,7 +161,7 @@ def main():
     INPUT_DATA_DIR = '/Users/pinesol/mlcs_data'
     OUTPUT_DATA_DIR = '/tmp'
     NUM_OPINION_SHARDS = 100 #1340
-    MIN_REQUIRED_COUNT = 10 #150
+    MIN_REQUIRED_COUNT = 2 #150
     USE_TFIDF = True
 
     # Model params
@@ -156,23 +169,31 @@ def main():
     REG_MIN_LOG10 = -2
     REG_MAX_LOG10 = 2
     SCORING = 'f1_weighted'
-    REGULARIZATION_TYPE='l1'
+    # NOTE: this will be too slow to run locally if feature reduction is enabled
+    FEATURE_REDUCTION_TYPE = None # TODO try 'chi2' or l1svc
 
     X, case_ids, y = jd.load_data(INPUT_DATA_DIR, OUTPUT_DATA_DIR,
                                   NUM_OPINION_SHARDS, MIN_REQUIRED_COUNT, USE_TFIDF)
 
+    # TODO write a loop for all of the params
     print 'Training and scoring models...'
     train_and_score_model(X, y, case_ids, 'baseline', train_pct=TRAIN_PCT, 
-                          reg_min_log10=None, reg_max_log10=None, scoring=None, regularization_type=None)
+                          reg_min_log10=None, reg_max_log10=None, scoring=None, feature_reduction_type=None)
     train_and_score_model(X, y, case_ids, 'logistic', train_pct=TRAIN_PCT,
-                          reg_min_log10=REG_MIN_LOG10, reg_max_log10=REG_MAX_LOG10, scoring=SCORING, regularization_type=REGULARIZATION_TYPE)
+                          reg_min_log10=REG_MIN_LOG10, reg_max_log10=REG_MAX_LOG10, scoring=SCORING, feature_reduction_type=FEATURE_REDUCTION_TYPE)
     train_and_score_model(X, y, case_ids, 'svm', train_pct=TRAIN_PCT,
-                          reg_min_log10=REG_MIN_LOG10, reg_max_log10=REG_MAX_LOG10, scoring=SCORING, regularization_type=REGULARIZATION_TYPE)
+                          reg_min_log10=REG_MIN_LOG10, reg_max_log10=REG_MAX_LOG10, scoring=SCORING, feature_reduction_type=FEATURE_REDUCTION_TYPE)
     train_and_score_model(X, y, case_ids, 'naive_bayes', train_pct=TRAIN_PCT,
-                          reg_min_log10=REG_MIN_LOG10, reg_max_log10=REG_MAX_LOG10, scoring=SCORING, regularization_type=REGULARIZATION_TYPE)
+                          reg_min_log10=REG_MIN_LOG10, reg_max_log10=REG_MAX_LOG10, scoring=SCORING, feature_reduction_type=FEATURE_REDUCTION_TYPE)
     train_and_score_model(X, y, case_ids, 'bernoulli_bayes', train_pct=TRAIN_PCT,
-                          reg_min_log10=REG_MIN_LOG10, reg_max_log10=REG_MAX_LOG10, scoring=SCORING, regularization_type=REGULARIZATION_TYPE)
+                          reg_min_log10=REG_MIN_LOG10, reg_max_log10=REG_MAX_LOG10, scoring=SCORING, feature_reduction_type=FEATURE_REDUCTION_TYPE)
 
+
+    # TODO P0 Make linear svc and chi2 options for feature reduction
+
+    # TODO P0 Make the baseline classifer work in the pipeline so it can take part in feature reduction.
+
+    # TODO P0 Make regularization type something that varies in the pipline
 
     # TODO P0 Create bar charts to visualize which classifiers are better
 
